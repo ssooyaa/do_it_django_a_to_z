1ec775309e29432494b480fd420265c4759cd05e
ssooyaa <tvfxq0214@gmail.com>
2022-08-10 17:04:25 +0900

    대문 페이지와 자기소개 페이지 만들기

--- single_pages/views.py
+++ single_pages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+def landing(request):
+    return render(
+        request,
+        'single_pages/landing.html',
+    )
+
+def about_me(request):
+    return render(
+        request,
+        'single_pages/about_me.html',
+    )
 # Create your views here.
