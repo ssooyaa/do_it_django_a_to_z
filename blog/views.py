5912cb434b434b9f7b9325cfd224e527c5901921
ssooyaa <tvfxq0214@gmail.com>
2022-08-10 16:28:46 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -11,4 +11,15 @@ def index(request):
         }
     )
 
+def single_post_page(request,pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post': post,
+        }
+    )
+
 # Create your views here.
