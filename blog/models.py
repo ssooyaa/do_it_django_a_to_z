28f0fc013331217c77fae870a78396d5e43b173d
ssooyaa <tvfxq0214@gmail.com>
2022-08-11 15:58:29 +0900

    첨부파일있을경우 포스트 상세페이지에서 다운로드 버튼

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,16 @@
 from django.db import models
-
+import os
 class Post(models.Model):
     title = models.CharField(max_length=50)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
+
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+
 
     #author:추후 작성 예정
     def __str__(self):
@@ -15,6 +18,12 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1]
 
 
 # Create your models here.
