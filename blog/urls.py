9b19684a3264fe7f5696d98425100057305fd171
ssooyaa <tvfxq0214@gmail.com>
2022-08-11 09:39:29 +0900

    CBV로 블로그 포스트 목록 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>/', views.single_post_page),
-    path('', views.index),
+    path('',views.PostList.as_view()),
+    #path('<int:pk>/', views.single_post_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-    )
-
-def single_post_page(request,pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
 # Create your views here.
