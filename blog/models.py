1574161ff65889fb93510122dc1b5d44601b42dd
ssooyaa <tvfxq0214@gmail.com>
2022-08-11 13:44:43 +0900

    포스트에 이미지 출력

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
 
     #author:추후 작성 예정
     def __str__(self):
--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,6 +120,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
