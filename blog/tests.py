f7f16e51b98b2a19566f8d1c026044902b82fe7e
ssooyaa <tvfxq0214@gmail.com>
2022-08-12 15:13:09 +0900

    포스트 상세 페이지 테스트

--- blog/tests.py
+++ blog/tests.py
@@ -35,4 +35,28 @@ class TestView(TestCase):
         self.assertIn(post_001.title, main_area.text)
         self.assertIn(post_002.title, main_area.text)
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+
+
+    def test_post_detail(self):
+        post_001 = Post.objects.create(
+            title='첫번째 포스트',
+            content='Hello World. We are the world.',
+        )
+
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+
+        response = self.client.get(post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About me', navbar.text)
+
+        self.assertIn(post_001.title,soup.title.text)
+
+        main_area = soup.find('div', id='main-area')
+        post_area = main_area.find('div', id='post-area')
+        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(post_001.content, post_area.text)
 # Create your tests here.
