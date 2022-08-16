94d364eca33d82350b0c51a740e5b73f27f3866f
ssooyaa <tvfxq0214@gmail.com>
2022-08-16 10:18:42 +0900

    작성자 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,20 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 class Post(models.Model):
     title = models.CharField(max_length=50)
     hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
 
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    #author:추후 작성 예정
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title}::{self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
