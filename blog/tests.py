10ed5e6d79a96f5c36421c521c5ffbebb9713e44
ssooyaa <tvfxq0214@gmail.com>
2022-08-16 09:34:18 +0900

    내비게이션 바와 푸터 모듈화

--- blog/tests.py
+++ blog/tests.py
@@ -6,14 +6,33 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About me', navbar.text)
+
+        logo_btn = navbar.find('a', text='스마트 부산')
+        self.assertEqual(logo_btn.attrs['href'],'/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+
+
+
     def test_post_list(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
-        self.assertEqual(soup.title.text, 'Blog')
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About me', navbar.text)
+        self.assertIn('Blog', soup.title.text)
+
+        self.navbar_test(soup)
+
         self.assertEqual(Post.objects.count(), 0)
         main_area = soup.find('div', id='main-area')
         self.assertIn('아직 게시물이 없습니다', main_area.text)
@@ -37,6 +56,8 @@ class TestView(TestCase):
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
 
 
+
+
     def test_post_detail(self):
         post_001 = Post.objects.create(
             title='첫번째 포스트',
@@ -49,9 +70,6 @@ class TestView(TestCase):
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About me', navbar.text)
 
         self.assertIn(post_001.title,soup.title.text)
 
