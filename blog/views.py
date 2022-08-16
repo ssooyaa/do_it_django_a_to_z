14d0046d704557fa4f6bc794bf6c28ce7cb17984
ssooyaa <tvfxq0214@gmail.com>
2022-08-16 16:50:14 +0900

    포스트 상세페이지 카테고리기능 추가

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,11 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     model = Post
+    def get_context_data(self, **kwargs):
+        context = super(PostDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
 
 
 # Create your views here.
