83de41cd2f174f1c4868b52d47df15e10f262daa
ssooyaa <tvfxq0214@gmail.com>
2022-08-19 15:33:17 +0900

    대문 페이지 수정

--- blog/models.py
+++ blog/models.py
@@ -56,5 +56,11 @@ class Post(models.Model):
     def get_content_markdown(self):
         return markdown(self.content)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/143/e3445497d896a175/svg/{{user.email}}'
+
 
 # Create your models here.
